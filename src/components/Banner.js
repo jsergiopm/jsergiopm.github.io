0ac6774c6b9a0e058c1c011a4886b50deab82f48
jsergiopm <sergioparra490@gmail.com>
2022-09-12 17:17:01 -0500

    Banner included

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,57 @@
-import { Col, Container, Row } from "react-bootstrap/esm"
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap/";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+        return () => {clearInterval(ticker)};
+
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
     return (
-        <section className="Banner" id="home">
+        <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <Col xs={12}>
-
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">This is my portfolio</span>
+                        <h1>{"Hi i'm jsergiopm "}<span className='wrap'>{text}</span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet mauris non nulla placerat mollis. Sed commodo, nisl non gravida faucibus, ante massa gravida urna, vitae iaculis massa est ac urna. Fusce commodo bibendum tristique. Nullam non tempus nibh. Sed dignissim erat auctor hendrerit maximus.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}></ArrowRightCircle></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img"></img>
                     </Col>
                 </Row>
             </Container>
