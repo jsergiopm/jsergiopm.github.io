4a85edbf7fffd0cacf69b21e0bf477a8ee7ed7bd
jsergiopm <sergioparra490@gmail.com>
2022-09-12 16:02:36 -0500

    Project created - Navbar added

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap/esm"
+
+export const Banner = () => {
+    return (
+        <section className="Banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12}>
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
